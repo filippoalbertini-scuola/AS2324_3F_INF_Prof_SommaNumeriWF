d63ff65b9b7ec897df64f8e1d76ffd80c5686c1c
Filippo Albertini <filippo@filippoalbertini.it>
2024-03-14 11:08:28 +0100

    Aggiungere i file di progetto.

---
+++ AS2324_3F_INF_Prof_SommaNumeriWF/Form1.cs
@@ -0,0 +1,10 @@
+namespace AS2324_3F_INF_Prof_SommaNumeriWF
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
